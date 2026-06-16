ec9e6d45cbe1d882dfc3ba7cc475886965a0ed37
mhmmadazrul <mhmmadazrul@gmail.com>
2026-06-16 19:52:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://backdfyzexrdokwmmsax.supabase.co/rest/v1/';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhY2tkZnl6ZXhyZG9rd21tc2F4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MDM5MzYsImV4cCI6MjA5NzE3OTkzNn0.rIK39UkiLM4cqVtkX915pG_-eWDBtCLPnxgm8UsgM9M';
+const SUPABASE_URL = https://backdfyzexrdokwmmsax.supabase.co/rest/v1/;
+const SUPABASE_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhY2tkZnl6ZXhyZG9rd21tc2F4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MDM5MzYsImV4cCI6MjA5NzE3OTkzNn0.rIK39UkiLM4cqVtkX915pG_-eWDBtCLPnxgm8UsgM9M;
 
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
